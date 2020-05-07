5db259a4ce5a4bbea84af49dd858ce638a72105f
Andrzej Schmidt <andrzej@gearedapp.co.uk>
2020-05-07 20:53:47 +0100

    :tada: Completed app

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,35 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+// class CoinData {
+//   getExchangeRate() async {
+//     NetworkHelper networkHelper = NetworkHelper(
+//         //'https://rest.coinapi.io/v1/exchangerate/BTC/USD?&apiKey=$apiKey'
+//         'https://blockchain.info/ticker');
+
+//     return await networkHelper.getData();
+//   }
+// }
+
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = '0DDC1C6A-A7AA-492C-B707-B62D4CAD42AF';
+
+class CoinData {
+  Future getCoinData(String crypto, String currency) async {
+    //String requestURL = 'https://blockchain.info/ticker';
+    String requestURL = '$coinAPIURL/$crypto/$currency?&apiKey=$apiKey';
+    http.Response response = await http.get(requestURL);
+
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      //var lastPrice = decodedData[currency]['last'];
+      var lastPrice = decodedData['rate'];
+
+      return lastPrice;
+    } else {
+      print(response.statusCode);
+
+      throw 'Problem with the get request, code ${response.statusCode}';
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'coin_data.dart';
+import 'dart:io' show Platform;
+import 'rate_card.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +10,75 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String bitcoinRate = '? ';
+  String ethereumRate = '? ';
+  String litecoinRate = '? ';
+
+  String selectedCurrency = currenciesList[0];
+  CoinData coinData = CoinData();
+
+  void initState() {
+    super.initState();
+    updateUI();
+  }
+
+  void updateUI() async {
+    dynamic dataBTC = await coinData.getCoinData('BTC', selectedCurrency);
+    dynamic dataETH = await coinData.getCoinData('ETH', selectedCurrency);
+    dynamic dataLTC = await coinData.getCoinData('LTC', selectedCurrency);
+
+    setState(() {
+      bitcoinRate = dataBTC.toStringAsFixed(2);
+      ethereumRate = dataETH.toStringAsFixed(2);
+      litecoinRate = dataLTC.toStringAsFixed(2);
+    });
+  }
+
+  CupertinoPicker iOSpicker() {
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) {
+        setState(() {
+          selectedCurrency = currenciesList[selectedIndex];
+        });
+        updateUI();
+      },
+      children: currenciesList
+          .map((currency) => Text(
+                currency,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ))
+          .toList(),
+    );
+  }
+
+  DropdownButton androidPicker() {
+    return DropdownButton<String>(
+      value: selectedCurrency,
+      //with for in loop
+      // items: [
+      //   for (String currency in currenciesList)
+      //     DropdownMenuItem(child: Text(currency), value: currency),
+      // ],
+      //or with map
+      items: currenciesList
+          .map(
+            (currency) =>
+                DropdownMenuItem(child: Text(currency), value: currency),
+          )
+          .toList(),
+      onChanged: (value) {
+        setState(() {
+          selectedCurrency = value;
+        });
+        updateUI();
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,33 +89,33 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              RateCard(
+                crypto: 'BTC',
+                rate: bitcoinRate,
+                selectedCurrency: selectedCurrency,
               ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
+              RateCard(
+                crypto: 'ETH',
+                rate: ethereumRate,
+                selectedCurrency: selectedCurrency,
               ),
-            ),
+              RateCard(
+                crypto: 'LTC',
+                rate: litecoinRate,
+                selectedCurrency: selectedCurrency,
+              ),
+            ],
           ),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: Platform.isIOS ? iOSpicker() : androidPicker(),
           ),
         ],
       ),
